22bc58378d8e507be908058feb977b258cc14487
Yacine Khamis <yacine.khamis@gmail.com>
2017-05-10 22:43:03 +0200

    Cleaning commit

--- polymerge.py
+++ polymerge.py
@@ -1,31 +1,10 @@
 """Polymerge tool."""
-from Test import Functions
 import sys
+from Test import Functions
+from polymerge import zipFunctions, fileFunctions
 
 if __name__ == '__main__':
-    print(Functions.test())
-
-    # First experiments with basic Python functions
-    b01 = Functions.createBytes(2)
-    Functions.printInfo(b01)
-
-    b02 = Functions.createBytes(255)
-    Functions.printInfo(b02)
-
-    i = 16
-    # Create four bytes from the integer
-    four_bytes = i.to_bytes(4, byteorder='big', signed=True)
-    Functions.printInfo(four_bytes)
-
-    # Compare the difference to little endian
-    Functions.printInfo(i.to_bytes(4, byteorder='little', signed=True))
-
-    # Play around with hexadecimal data
-    hexaData = Functions.b.hexlify(b'\x00\xff')
-    Functions.printInfo(hexaData)
-
-    # Trying to open a file and print its content in binary mode
-    with open(sys.argv[1], 'rb') as file:
-        firstFourBytes = file.read(4)
-
-    Functions.printInfo(firstFourBytes)
+    # file = zipFunctions.createZipFile(sys.argv[1])
+    # print(fileFunctions.stats('./tmp/fakePublicKey.txt.zip'))
+    # fileFunctions.printHexa(sys.argv[1])
+    fileFunctions.printHexa('./tmp/special_image.zip')
