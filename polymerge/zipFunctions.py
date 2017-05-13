c5c5a9f367bf992ddf9b177c6233af2133bc5b7a
Yacine Kha <yacine.khamis@gmail.com>
2017-05-13 21:35:39 +0200

    Final commit. Removing all other files. This tool could now be used.

--- polymerge/zipFunctions.py
+++
@@ -1,8 +0,0 @@
-"""Create a zip archive from specified file."""
-import zipfile
-
-
-def createZipFile(fileToArchive):
-    path = './tmp/' + fileToArchive + '.zip'
-    with zipfile.ZipFile(path, mode='w') as myZip:
-        myZip.write(fileToArchive)
